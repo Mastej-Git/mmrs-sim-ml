ba1b05c2d38229d1c0b51497d6c475b9b889d016
Michal Mastej <m.mastej1812@gmail.com>
2026-02-20 00:35:57 +0100

    Added sectors division pipeline code

--- src/mmrs_sim_ml/pipelines/sector_division/pipeline.py
+++ src/mmrs_sim_ml/pipelines/sector_division/pipeline.py
@@ -3,8 +3,20 @@ This is a boilerplate pipeline 'sector_division'
 generated using Kedro 1.2.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+from kedro.pipeline import Pipeline, Node
+
+from .nodes import detect_col_sectors
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        Node(
+            func=detect_col_sectors,
+            inputs={
+                "agvs": "params:agvs",
+                "bezier_points": "bezier_points",
+            },
+            outputs="collision_sectors",
+            name="detect_col_sectors_node",
+        ),
+    ])
